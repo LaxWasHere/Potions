575434bee87d4c9791b9253a522cb9529e13beb1
LaxWasHere <public@LaxWasHere.com>
2013-01-09 17:45:51 -0800

    Revert "Fixed neutral potions"
    This reverts commit 337b81352ddc8ba763e1ed4eb1c828902fd70dd3.

--- src/net/lazlecraft/potions/neutral.java
+++ src/net/lazlecraft/potions/neutral.java
@@ -3,7 +3,6 @@ package net.lazlecraft.potions;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -14,41 +13,14 @@ public class neutral implements Listener {
 	@EventHandler
     public void onJoin(PlayerJoinEvent event){
         Player p = event.getPlayer();
-        if(p.hasPermission("potions.neutral.invisible")) {
-        	System.out.println("OJ INVI");
-         p.removePotionEffect(PotionEffectType.INVISIBILITY);
-            p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 18000, 1));
+        //Invisibility
+        if(p.hasPermission("potions.invisible")) {
+            p.removePotionEffect(PotionEffectType.INVISIBILITY);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 18000, 0));
         }
-        if(p.hasPermission("potions.neutral.nightvision")) {
-        	System.out.println("OJ INVI");
+        if(p.hasPermission("potions.nightvision")); {
         	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-            p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 18000, 1));
+        	p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 18000, 0));
         }
-        if(p.hasPermission("potions.neutral.aqua")) {
-        	System.out.println("OJ INVI");
-        	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-            p.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 18000, 1));
-        }
-	}
-	
-        @EventHandler
-        public void onInventoryClose(InventoryCloseEvent event){
-            Player p = (Player) event.getPlayer();
-            //Invisible
-            if(p.hasPermission("potions.neutral.invisible")) {
-            	System.out.println("IC INVI");
-             p.removePotionEffect(PotionEffectType.INVISIBILITY);
-                p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 18000, 1));
-            }
-            if(p.hasPermission("potions.neutral.nightvision")) {
-            	System.out.println("IC INVI");
-            	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 18000, 1));
-            }
-            if(p.hasPermission("potions.neutral.aqua")) {
-            	System.out.println("IC INVI");
-            	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-                p.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 18000, 1));
-           }
-     }
+	}	
 }
